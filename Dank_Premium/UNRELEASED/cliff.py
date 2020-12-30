9ad0b8ff8cb1a590165b68d529061e550b6e0a4b
Noah <59535872+xxXSniproXxxNoahCx@users.noreply.github.com>
2020-12-30 14:08:27 -0500

    Update cliff.py

--- Dank_Premium/UNRELEASED/cliff.py
+++ Dank_Premium/UNRELEASED/cliff.py
@@ -1,14 +1,14 @@
 import random
 from telegram.ext import Dispatcher,CommandHandler
-import balence
+import bal
 
 def c(update, context):
     ri = random.randint(2000,12000)
-    choices = ["You accidentally fell of the cliff and broke both of your legs.", "You fell of the cliff and landed hard on a rock.", "You successfully went down safely. You examined the plants and reported it to the scientests. The scientests had never seen such things before and you are rewarded %s XP for a reward."%(ri)]
+    choices = ["You accidentally fell of the cliff and broke both of your legs.", "You fell of the cliff and landed hard on a rock.", "You successfully went down safely. You examined the plants and reported it to the scientests. The scientests had never seen such things before and you are rewarded $%s for a reward."%(ri)]
     msg = random.choice(choices)
-    msg += "\n\nAuthorised By Noah <3\n作者：Noah"
+    msg += "\n\nᴀᴜᴛʜᴏʀɪꜱᴇᴅ ʙʏ ɴᴏᴀʜ ❤️ \n作者：ɴᴏᴀʜ"
     update.message.reply_text(msg)
 
 def add_handler(dp:Dispatcher):
-    w_handler = CommandHandler('DCliff', c)
+    w_handler = CommandHandler('PDCliff', c)
     dp.add_handler(w_handler)
