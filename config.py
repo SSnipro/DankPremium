977dd8b104c0d7255cc19c2a4629b3d063c93962
xxX☆Snipro☆Xxx ☆〜（ゝ。∂）#TheMythicNoahC <59535872+xxXSniproXxxNoahCx@users.noreply.github.com>
2020-12-29 20:22:48 -0500

    Config Update!

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def load_config():
     with open(config_file, 'r') as configfile:
@@ -9,6 +10,7 @@ def save_config():
     with open(config_file, 'w') as configfile:
         json.dump(CONFIG, configfile, indent=4,ensure_ascii=False)
 
+RUN_PATH = os.getcwd()
 
 config_file = 'my.json'
 CONFIG = load_config()
