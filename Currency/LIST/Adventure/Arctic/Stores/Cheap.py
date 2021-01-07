859926b9ca579e3e990b3d7524009939e9cf5aa9
xxX☆Snipro☆Xxx ☆〜（ゝ。∂）#TheMythicNoahC <59535872+xxXSniproXxxNoahCx@users.noreply.github.com>
2021-01-06 20:50:57 -0500

    cheap update

--- Currency/LIST/Adventure/Arctic/Stores/Cheap.py
+++ Currency/LIST/Adventure/Arctic/Stores/Cheap.py
@@ -3,8 +3,14 @@ from telegram.ext import Dispatcher,CommandHandler
 from Currency import bal
 
 def cheap(update, context):
-    shopping = ["You bought fake supplies. Get rekt.", "DRILL NO WORK????????", "You made the right choice, good call! Got to your friend's research base and earned 7777 XP."]
+    user = update.effective_user
+    shopping = [
+        "💥 You bought fake supplies. Get rekt.", 
+        "🚱 DRILL NO WORK????????", 
+        "✅ You made the right choice, good call! Got to your friend's research base and earned $7777."]
     msg = random.choice(shopping)
+    if msg == shopping[2]:
+        bal.addcoins(user,7777)
     msg += "\n\n\n\nᴀᴜᴛʜᴏʀɪꜱᴇᴅ ʙʏ ɴᴏᴀʜ ❤️ \n作者：ɴᴏᴀʜ"
     update.message.reply_text(msg)
 
