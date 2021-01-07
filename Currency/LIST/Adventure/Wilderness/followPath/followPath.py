47735c2719f3e07274fcbb3d06b6be0644755045
xxX☆Snipro☆Xxx ☆〜（ゝ。∂）#TheMythicNoahC <59535872+xxXSniproXxxNoahCx@users.noreply.github.com>
2021-01-06 20:53:23 -0500

    Update followPath.py

--- Currency/LIST/Adventure/Wilderness/followPath/followPath.py
+++ Currency/LIST/Adventure/Wilderness/followPath/followPath.py
@@ -7,9 +7,7 @@ def fp(update, context):
     ok = random.randint(1111,3333)
     sp = ["temple 🛕. You found alot of ancient redstone contrapions 🎛 and sold them for $%s."%(ok), "dead end 💀.", "pond and a shop beside it. 🌊 If you want to buy a fishing rod and fish, use the command /PDFish 🎣."]
     d = random.choice(sp)
-    msg44 = """You followed the path and found a %s
-
-"""%(d)
+    msg44 = "You followed the path and found a %s"%(d)
     if d == sp[0]:
         bal.addcoins(user,ok)
     msg44 += "\n\nᴀᴜᴛʜᴏʀɪꜱᴇᴅ ʙʏ ɴᴏᴀʜ ❤️ \n作者：ɴᴏᴀʜ"
